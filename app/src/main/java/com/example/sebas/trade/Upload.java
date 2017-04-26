6ef4f8932d7c4e302f72982550f4411046e8cb8e
Sebastien <sebastienlindor@gmail.com>
2017-04-25 20:38:36 -0400

    Added bottom bar to Upload page to allow user to continue navigating. May add upload button to menu bar

--- app/src/main/java/com/example/sebas/trade/Upload.java
+++ app/src/main/java/com/example/sebas/trade/Upload.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -56,6 +59,25 @@ public class Upload extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
+//        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+//                findViewById(R.id.bottomNavView_Bar);
+//        BottomNavigationHelper.disableShiftMode(bottomNavigationView);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        switch (item.getItemId()){
+//                            default:
+//                                break;
+//                        }
+//
+//
+//
+//                        return false;
+//                    }
+//                });
+
         mStorage = FirebaseStorage.getInstance().getReference();
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Post");
 
@@ -84,6 +106,25 @@ public class Upload extends AppCompatActivity {
                 startPosting();
             }
         });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationHelper.disableShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()){
+                            default:
+                                break;
+                        }
+
+
+
+                        return false;
+                    }
+                });
     }
 
     private void startPosting() {
